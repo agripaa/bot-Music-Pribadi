e5a5454bce0a3d2d762e19d8e28e0bf21409accd
Jounnyy <agrieva.xananda08@gmail.com>
2022-05-09 22:03:10 +0700

    fixed play

--- music.py
+++ music.py
@@ -1,7 +1,7 @@
 import discord
 import youtube_dl
 import asyncio
-import pafy
+from youtube_dl import YoutubeDL
 from discord.ext import commands
 
 
@@ -9,11 +9,27 @@ class Player(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.song_queue = {}
+        self.music_queue = []
+        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+        self.MMPEG_OPTIONS = {
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        self.is_playing = False
+        self.is_paused = False
+        self.vc = None
         self.setup()
 
     def setup(self):
         for guild in self.bot.guilds:
             self.song_queue[guild.id] = []
+
+    def search_yt(self, item):
+        with YoutubeDL(self.YDL_OPTIONS) as ydl:
+            try:
+                info = ydl.extract_info("ytsearch:%s" %
+                                        item, download=False)['entries'][0]
+            except Exception:
+                return False
+        return {'source': info['formats'][0]['url'], 'title': info['title']}
 
     async def check_queue(self, ctx):
         if len(self.song_queue[ctx.guild.id]) > 0:
@@ -27,11 +43,35 @@ class Player(commands.Cog):
             return None
         return [entry["webpage_url"] for entry in info["entries"]] if get_url else info
 
-    async def play_song(self, ctx, song):
-        url = pafy.new(song).getbestaudio().url
-        ctx.voice_client.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(
-            executable="C:/path/ffmpeg.exe", source="mp3.mp3")), after=lambda error: self.bot.loop.create_task(self.check_queue(ctx)))
-        ctx.voice_client.source.volume = 0.5
+    async def play_next(self):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+            m_url = self.music_queue[0][0]['source']
+            self.music_queue.pop(0)
+            self.vc.play(discord.FFmpegPCMAudio(
+                m_url, **self.MMPEG_OPTIONS), after=lambda e: self.play_next())
+        else:
+            self.is_playing = False
+
+    async def play_song(self, ctx):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+            m_url = self.music_queue[0][0]['source']
+
+            if self.vc == None or not self.vc.is_connected():
+                self.vc = await self.music_queue[0][1].connect()
+
+                if self.vc == None:
+                    await ctx.send("Paham sopan santun kek anj, minimal gw masukin ke room dlu sat!")
+                    return
+
+            else:
+                await self.vc.move_to(self.music_queue[0][1])
+                self.music_queue.pop(0)
+                self.vc.play(discord.FFmpegPCMAudio(
+                    m_url, **self.MMPEG_OPTIONS), after=lambda e: self.play_next())
+        else:
+            self.is_playing = False
 
     @commands.command()
     async def join(self, ctx):
@@ -48,35 +88,31 @@ class Player(commands.Cog):
         await ctx.send("Bye bitch")
 
     @commands.command()
-    async def play(self, ctx, *, song=None):
-        if song is None:
-            return await ctx.send("mau denger musik lagi gak ngaf? buru req ajim")
-        if ctx.voice_client is None:
-            return await ctx.send("Lu kalau mau req musik minimal tau etika lah anj! masukin gw dlu ke room")
-        # handle a song where song in url
-        if not ("youtube.com/watch?" in song or "https://youtu.be/" in song):
-            await ctx.send("bentar dlu gw cari lagu yang lu maksud")
-            result = await self.search_song(1, song, get_url=True)
-            if result is None:
-                return await ctx.send("duh gw gak nemu lagu yang lu maksud, coba masukin judul sama penyanyinya nyet!")
-            song = result[0]
-        if ctx.voice_client.source is not None:
-            queue_len = len(self.song_queue[ctx.guild.id])
-            if queue_len < 10:
-                self.song_queue[ctx.guild.id].append(song)
-                return await ctx.send(f"gwejh lagi muterin lagu nyet sabar tunggu giliran kira kira nomor urut: {queue_len+1}")
-            else:
-                return await ctx.send("udah woi gw cuman bisa kuat nampung 10 lagu jangan banyak banyak tunggu satu lagu selesai dlu baru req lagi")
+    async def play(self, ctx, *args):
+        query = " ".join(args)
+        voice_channel = ctx.author.voice.channel
 
-        await self.play_song(ctx, song)
-        await ctx.send(f"Sekarang lagi muterin : {song}")
+        if voice_channel is None:
+            await ctx.send("masukkan gweh dlu banh dalam room asu!")
+        elif self.is_paused:
+            self.vc.resume()
+        else:
+            song = self.search_yt(query)
+            if type(song) == type(True):
+                await ctx.send("Lagu yang luwh maksud gak ada di database, coba pake link youtube atau ketik yang bener jangan nyatir!")
+            else:
+                await ctx.send("dah nih lagu gw masukin list selamat mendengarkan nyet!")
+                self.music_queue.append([song, voice_channel])
+
+                if self.is_playing == False:
+                    await self.play_song(ctx)
 
     @commands.command()
     async def search(self, ctx, *, song=None):
         if song is None:
             return await ctx.send("masukin lagunya jangan lupa nyet!")
         await ctx.send("bentar gw lagi cari dlu lagunya yang lu maksud")
-        info = await self.search_song(7, song)
+        info = await self.search_song(10, song)
         embed = discord.Embed(
             title=f"hasil lagu yang lu cari : '{song}':", description="*lu bisa gunain link untuk req kalau misalkan lagu yang lu maksud gak sesuai keinginan lu. *\n", colour=discord.Colour.red())
         amount = 0
